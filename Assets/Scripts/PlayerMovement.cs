40eea4bf3b2126ab07496c9feb2cd48919231bb8
Victor-Segrelles <106614616+Victor-Segrelles@users.noreply.github.com>
2022-11-05 16:27:08 +0100

    level 1 and colliders
    cambiado movimiento de transform a rigidbody, configurado fixed update, collides transparentes funcionales

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,23 @@ public class PlayerMovement : MonoBehaviour
     public float speed =10f;
     Vector2 lastClick;
     bool moving;
-    private void Update() {
+    public Rigidbody2D rigidbody2d;
+    void start(){
+        rigidbody2d=GetComponent<Rigidbody2D>();
+    }
+    void Update() {
         if (Input.GetMouseButtonDown(0)){
             lastClick=Camera.main.ScreenToWorldPoint(Input.mousePosition);
             moving=true;
         }
-        if(moving && (Vector2)transform.position !=lastClick){
+    }
+    void FixedUpdate(){
+        if(moving && (Vector2)rigidbody2d.position !=lastClick){
             float step=speed*Time.deltaTime;
-            transform.position=Vector2.MoveTowards(transform.position, lastClick, step);
+            //transform.position=Vector2.MoveTowards(transform.position, lastClick, step);
+            ///Vector2 direction=(lastClick - transform.position).normalized;
+            //rigidbody2d.AddForce(direction);
+            rigidbody2d.MovePosition(Vector2.MoveTowards(rigidbody2d.position, lastClick, step));
         } else {
             moving=false;
         }
---
+++ Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    public GameObject player;
+    void LateUpdate(){
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+    }
+}
