e3d9c483c6ee61fa8cc4cc178f4c78faed561810
izankuni <al404504@uji.es>
2022-11-28 11:48:33 +0100

    Minijuego del guacamole con puntuación y tiempo
    Paso de escribir los títulos en inglés que parezco guiri.  La puntuación y el tiempo funciona, he creado un nuevo objeto ya que el script estaba en la cámara

--- Assets/Scripts/Minigames/Whack-a-virus/ViruSpawn.cs
+++ Assets/Scripts/Minigames/Whack-a-virus/ViruSpawn.cs
@@ -2,26 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ViruSpawn : MonoBehaviour
 {
     public GameObject virusPrefab;
     public Transform[] spawnPoints;
-
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI timeText;
+    public int scoreNum;
+    private float initialTime = 0;
+    public int showTime;
 
     GameObject virus;
     //Vector2 virusPos;
 
-
     // Start is called before the first frame update
     void Start()
     {
+        scoreNum = 0;
         Spawn();
+        ShowScore();
+        ShowTime();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (initialTime >= 1)
+        {
+            initialTime = 0;
+            showTime--;
+            ShowTime();
+            if (showTime == 0)
+                SceneManager.LoadScene("GameOver"); //Game Over como ejemplo, se cambiará a la escena correspondiente
+        }
+        else
+        {
+            initialTime += Time.deltaTime;  
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
             //Transforma las unidades de las coordenadas del ratón
@@ -30,10 +52,19 @@ public class ViruSpawn : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
             
             // Si el rayo detecta algo
-            if (hit.collider != null)
+            if (hit.collider != null)  
             {
-                GameObject.Destroy(hit.transform.gameObject);
-                Spawn();
+                if (hit.collider.gameObject.CompareTag("Virus"))
+                {
+                    GameObject.Destroy(hit.transform.gameObject);
+                    Spawn();
+                    scoreNum++;
+                }
+                else
+                {
+                    scoreNum--;
+                }
+                ShowScore();
             }
         }
     }
@@ -45,7 +76,17 @@ public class ViruSpawn : MonoBehaviour
         virus.transform.position = spawnPoints[Random.Range(0,spawnPoints.Length)].transform.position;
         //virusPos =  virus.transform.position;
     }
-    
 
-   
+    public void ShowScore()
+    {
+        scoreText.text = "Puntos: " + scoreNum;
+    }
+
+    public void ShowTime()
+    {
+        timeText.text = "Tiempo: " + showTime;
+    }
+
+
+
 }
