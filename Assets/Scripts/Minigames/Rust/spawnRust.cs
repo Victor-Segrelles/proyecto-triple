8b47fbf2885a2425f92dbfe052c76d53bb106a08
izankuni <al404504@uji.es>
2022-12-06 23:46:10 +0100

    Medio solución al minijuego del óxido
    Basicamente el problema es el tamaño, es decir generá tantos puntos que se chocan, están fuera etc que peta. Una solución seria hacerlos pseudo aleatorios. De momento solo hay números sueltos

--- Assets/Scripts/Minigames/Rust/spawnRust.cs
+++ Assets/Scripts/Minigames/Rust/spawnRust.cs
@@ -17,6 +17,12 @@ public class spawnRust : MonoBehaviour
     int rustAmount;
     int charNum;
 
+    Vector2 arrowPosition;
+    RaycastHit2D markHit;
+    RaycastHit2D markHitG;
+    RaycastHit2D rustHit;
+    RaycastHit2D rustHitG;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,25 +32,23 @@ public class spawnRust : MonoBehaviour
             character[i].gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
         }
         charNum = 0;
-        setCharacter(charNum);
+        SetCharacter(charNum);
         
     }
 
     // Update is called once per frame<>
     void Update()
     {
-        
         if (rustAmount <= 0)
         {
             character[charNum].SetActive(false);
-            
-            
             charNum++;
             
             if (charNum < character.Length)
             {
+                Debug.Log("K");
                 //character[charNum].SetActive(true);
-                setCharacter(charNum);
+                SetCharacter(charNum);
             }
             else 
             {
@@ -56,64 +60,61 @@ public class spawnRust : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-                destroyIfHit();
+                DestroyIfHit();
             }
         }
-       
-            
-
-
     }
 
     //controlar que el oxido aparezca en sitios aleatorios DENTRO del personaje
-    void generateRust(int cantidad)
+    void GenerateRust(int cantidad)
     {
         int i = 0;
         while (i < cantidad)
         {
-            RaycastHit2D rustHit = Physics2D.Raycast(rustPosition, Vector2.zero);
+            Debug.Log("F");
+            RaycastHit2D rustHitG = Physics2D.Raycast(rustPosition, Vector2.zero);
 
             // Si el rayo detecta algo 
-            if (rustHit.collider != null)
+            if (rustHitG.collider != null)
             {
-
+                Debug.Log("G");
                 markPosition = new Vector3(barra.transform.position.x, rustPosition.y, 0f);
 
-                RaycastHit2D markHit = Physics2D.Raycast(markPosition, Vector2.zero);
+                RaycastHit2D markHitG = Physics2D.Raycast(markPosition, Vector2.zero);
 
                 //miro si ya había una marca en la barra para evitar que se superpongan, estén en la misma coord x o estén demasiado pegadas en la coord y
-                if (markHit.collider == null)
+                if (markHitG.collider == null)
                 {
+                    Debug.Log(rustPosition);
                     Instantiate(rustPrefab, rustPosition, Quaternion.identity);
                     Instantiate(markPrefab, markPosition, Quaternion.identity);
                     i++;
                 }
-
             }
-
-            rustPosition = new Vector3(Random.Range(-7f, 7f), Random.Range(-4f, 4f), 0f);
+            rustPosition = new Vector3(Random.Range(0f, 5f), Random.Range(-3f, 3f), 0f);
+            Debug.Log("I");
         }
     }
 
     //El personaje que aparezca se rellena con oxido y cambia de capa para que no se tenga en cuenta en los raycast
-    void setCharacter(int i)
+    void SetCharacter(int i)
     {
         character[i].SetActive(true);
         character[i].gameObject.layer = LayerMask.NameToLayer("Default");
         rustPosition = new Vector3(Random.Range(-7f, 7f), Random.Range(-4f, 4f), 0f);
 
         rustAmount = Random.Range(3, 5);
-        generateRust(rustAmount);
+        GenerateRust(rustAmount);
         character[i].gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
     }
     
 
     //Se lanza un rayo desde la posicion de la flecha. Si golpea una marca, le coloca la capa de Ignore Raycast y, si hay oxido, destruye ambos.
     //Si no hay oxido (que puede ocurrir dado que la colision de la marca es mas grande que la propia marca para evitar que spawneen pegadas) simplemente le devuelve a marca su capa original)
-    void destroyIfHit()
+    void DestroyIfHit()
     {
-        Vector2 arrowPosition = flecha.transform.position;
-        RaycastHit2D markHit = Physics2D.Raycast(arrowPosition, Vector2.right);
+        arrowPosition = flecha.transform.position;
+        markHit = Physics2D.Raycast(arrowPosition, Vector2.right);
         //Debug.DrawRay(arrowPosition, transform.forward * 10, Color.green, 1.0f);
 
         if (markHit.collider != null)
@@ -121,16 +122,12 @@ public class spawnRust : MonoBehaviour
             if (markHit.collider.gameObject.CompareTag("Marca"))
             {
                 markHit.collider.gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
-                RaycastHit2D rustHit = Physics2D.Raycast(arrowPosition, Vector2.right);
-                if (rustHit.collider != null)
+                rustHit = Physics2D.Raycast(arrowPosition, Vector2.right);
+                if (rustHit.collider != null && rustHit.collider.gameObject.CompareTag("Oxido"))
                 {
-                    if (rustHit.collider.gameObject.CompareTag("Oxido"))
-                    {
-                        GameObject.Destroy(markHit.transform.gameObject);
-                        GameObject.Destroy(rustHit.transform.gameObject);
+                        Destroy(markHit.transform.gameObject);
+                        Destroy(rustHit.transform.gameObject);
                         rustAmount--;
-                    }
-                    
                 }
                 else
                 {
@@ -140,8 +137,6 @@ public class spawnRust : MonoBehaviour
 
         }
     }
-
-    
 }
 
 /*Vector3 arrowPosition = new Vector3(flecha.transform.position.x, flecha.transform.position.y, 0f);
