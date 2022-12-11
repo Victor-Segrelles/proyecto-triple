f4e25a1f443ba0d0a3922092ad8128ab92cce1ff
izankuni <al404504@uji.es>
2022-12-11 20:50:16 +0100

    Tutoriales en los minijuegos
    Falta el del hielo que hay discusión

---
+++ Assets/Scripts/Minigames/TutorialScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartMinigame()
+    {
+        Destroy(gameObject);
+        Time.timeScale = 1;
+    }
+}
