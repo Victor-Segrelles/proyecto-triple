455dd054bcfd9a455ba440831bb7e2a517ed62c8
izankuni <al404504@uji.es>
2023-01-24 13:37:12 +0100

    He arreglado un par de cosas
    Espero que salga bien el juego a tiempo

---
+++ Assets/Scripts/FadeToBlack.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeToBlack : MonoBehaviour
+{
+    public GameObject blackScreen;
+    int fspeed = 1;
+    Color objectColor;
+    float fadeAmount = 0;
+    bool startFading;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        blackScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(startFading)
+        {
+            fadeAmount = objectColor.a + fspeed * Time.deltaTime;
+            Debug.Log(fadeAmount);
+
+            objectColor = new Color(0, 0, 0, fadeAmount);
+            blackScreen.GetComponent<Image>().color = objectColor;
+            if (fadeAmount > 1)
+            {
+                startFading = false;
+                SceneManager.LoadScene("Ciudad");
+            }
+                
+        }
+
+    }
+
+    public void FadeBlack()
+    {
+        blackScreen.SetActive(true);
+        startFading = true;
+    }
+}
