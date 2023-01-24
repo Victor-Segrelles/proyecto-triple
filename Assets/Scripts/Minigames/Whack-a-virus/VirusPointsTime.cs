969a1d9b3fa6acbc1f238b720777c3a358a4205a
izankuni <al404504@uji.es>
2023-01-24 16:35:15 +0100

    Mejoras visuales
    Ahora por la tarde empiezo el tema de las misiones

--- Assets/Scripts/Minigames/Whack-a-virus/VirusPointsTime.cs
+++ Assets/Scripts/Minigames/Whack-a-virus/VirusPointsTime.cs
@@ -8,9 +8,6 @@ public class VirusPointsTime : MonoBehaviour
 {
     [SerializeField] public TextMeshProUGUI scoreText;
     [SerializeField] public TextMeshProUGUI timeText;
-    [SerializeField] public GameObject grietas1;
-    [SerializeField] public GameObject grietas2;
-    [SerializeField] public GameObject grietas3;
     [SerializeField] private GameObject GameOverPanel;
     public int scoreNum;
     public int showTime;
@@ -26,9 +23,6 @@ public class VirusPointsTime : MonoBehaviour
         GameOverPanel.SetActive(false);
         GetInstance().ShowScore();
         timeText.text = "Tiempo: " + showTime;
-        grietas1.SetActive(false);
-        grietas2.SetActive(false);
-        grietas3.SetActive(false);
     }
 
     // Update is called once per frame
@@ -43,18 +37,6 @@ public class VirusPointsTime : MonoBehaviour
     public void ShowScore()
     {
         scoreText.text = "Puntos: " + scoreNum;
-        if (scoreNum == 10)
-            grietas1.SetActive(true);
-        if (scoreNum == 20)
-        {
-            grietas1.SetActive(false);
-            grietas2.SetActive(true);
-        }
-        if(scoreNum == 29)
-        {
-            grietas2.SetActive(false);
-            grietas3.SetActive(true);
-        }
 
     }
 
